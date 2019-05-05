7703def074e456104158c38bb1545c37723b1fda
Mr Thinh <maitron1niemtin@gmail.com>
2019-05-05 20:42:57 +0700

    a

--- Api.Dynamic/CacheService.cs
+++ Api.Dynamic/CacheService.cs
@@ -11,7 +11,9 @@ namespace Api.Dynamic
     {
         public bool Ok { set; get; }
         public string SendId { set; get; }
+        public string SearchId { set; get; }
         public string Code { set; get; }
+        public string Conditions { set; get; }
 
         [JsonIgnore]
         public string Output { set; get; }
@@ -22,6 +24,16 @@ namespace Api.Dynamic
             this.SendId = sendId;
             this.Code = code;
             this.Output = string.Empty;
+            this.SearchId = string.Empty;
+        }
+
+        public oCacheSearchResult(string sendId, string cacheId)
+        {
+            this.Ok = false;
+            this.SendId = sendId;
+            this.Code = "SEARCH";
+            this.SearchId = cacheId;
+            this.Output = string.Empty;
         }
 
         public string toJson()
@@ -30,6 +42,32 @@ namespace Api.Dynamic
         }
     }
 
+    public class oCacheSearchField
+    {
+        public int Index { set; get; }
+        public string Field { set; get; }
+        public string Condition { set; get; }
+        public string OrderBy { set; get; }
+        public string SearchId { set; get; }
+    }
+
+    public class oCacheSearchFieldRseult
+    {
+        public bool Ok { set; get; }
+        public int Index { set; get; }
+        public string SearchId { set; get; }
+        public int[] Output { set; get; }
+        public string Message { set; get; }
+
+        public oCacheSearchFieldRseult()
+        {
+            this.Ok = false;
+            this.Index = 0;
+            this.SearchId = string.Empty;
+            this.Output = new int[] { };
+            this.Message = string.Empty;
+        }
+    }
 
     public class CacheServiceFactory : ServiceHostFactory
     {
@@ -43,42 +81,100 @@ namespace Api.Dynamic
 
     public class CacheService : WebSocketService
     {
-        private static ConcurrentDictionary<string, WebSocketService> storeFields = new ConcurrentDictionary<string, WebSocketService>() { };
-        private static string storeIDs = string.Empty;
+        private static ConcurrentDictionary<string, WebSocketService> m_storeFields = new ConcurrentDictionary<string, WebSocketService>() { };
+        private static string m_storeIDs = string.Empty;
+        private static string[] m_fields = new string[] { };
+
+        private static ConcurrentDictionary<string, WebSocketService> m_searchSender = new ConcurrentDictionary<string, WebSocketService>() { };
+        private static ConcurrentDictionary<string, oCacheSearchFieldRseult> m_searchItemResults = new ConcurrentDictionary<string, oCacheSearchFieldRseult>() { };
+        private static ConcurrentDictionary<string, int> m_searchItemTotal = new ConcurrentDictionary<string, int>() { };
+        private static ConcurrentDictionary<string, int> m_searchItemCounter = new ConcurrentDictionary<string, int>() { };
 
         void fetchCacheItems(string conditions)
         {
+            try
+            {
+                oCacheSearchField[] searchs = JsonConvert.DeserializeObject<oCacheSearchField[]>(conditions);
+                if (searchs.Length > 0)
+                {
+                    string searchId = Guid.NewGuid().ToString();
 
+                    m_searchSender.TryAdd(searchId, this);
+                    m_searchItemTotal.TryAdd(searchId, searchs.Length);
+
+                    string find; WebSocketService cache;
+                    for (int i = 0; i < searchs.Length; i++)
+                    {
+                        searchs[i].SearchId = searchId;
+                        find = JsonConvert.SerializeObject(searchs[i]);
+                        if (m_storeFields.TryGetValue(searchs[i].Field, out cache)) {
+                            try
+                            {
+                                cache.Send(find);
+                            }
+                            catch(Exception exx) {
+                                m_searchItemResults.TryAdd(searchId + searchs[i].Index, new oCacheSearchFieldRseult() {
+                                    Ok = false,
+                                    Message = exx.Message,
+                                    Index = searchs[i].Index,
+                                    SearchId = searchId
+                                });
+
+                                if (m_searchItemCounter.ContainsKey(searchId)) {
+                                    int counter = 0;
+                                    if (m_searchItemCounter.TryGetValue(searchId, out counter))
+                                        m_searchItemCounter.TryUpdate(searchId, counter + 1, counter);
+                                } else {
+                                    m_searchItemCounter.TryAdd(searchId, 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) {
+                this.Send(new oCacheSearchResult(false, "EXCEPTION", this.WebSocketContext.SecWebSocketKey) { Output = ex.Message, Conditions = conditions }.toJson());
+            }
         }
 
-        void processMessage(string message)
+        void processMessage(string m)
         {
-            // Register TABLE.FIELD_NAME by text is #TABLE.FIELD_NAME
-            if (message.Length > 0 && message[0] == '#')
-            {
-                string field = message.Substring(1).ToUpper().Trim();
-                if (storeFields.ContainsKey(field))
-                    storeFields[field] = this;
-                else
-                    storeFields.TryAdd(field, this);
-                lock (storeIDs) storeIDs += ";" + this.WebSocketContext.SecWebSocketKey;
-            }
-            else
-            {
-                bool isReturnFetchCacheItems = false;
-                lock (storeIDs) isReturnFetchCacheItems = storeIDs.Contains(this.WebSocketContext.SecWebSocketKey);
-                // Response return from clients cache data executed query
-                if (isReturnFetchCacheItems)
-                {
+            if (string.IsNullOrWhiteSpace(m)) return;
+            m = m.Trim();
+            if (m.Length < 2) return;
 
-                }
-                else
-                {
-                    if (message.ToLower() == "table")
-                        this.Send(new oCacheSearchResult(true, "TABLE", this.WebSocketContext.SecWebSocketKey) { Output = string.Join(";", storeFields.Keys) }.toJson());
-                    else
-                        fetchCacheItems(message);// send BroadCast query data from end-user
-                }
+            //Send BroadCast query data from end-user
+            if (m[0] == '[' && m[m.Length - 1] == ']')
+            {
+                fetchCacheItems(m);
+                return;
+            }
+
+            string code = m.ToUpper();
+            switch (code)
+            {
+                case "TABLE":
+                    this.Send(new oCacheSearchResult(true, code, this.WebSocketContext.SecWebSocketKey) { Output = JsonConvert.SerializeObject(m_fields) }.toJson());
+                    break;
+                default:
+                    switch (m[0])
+                    {
+                        case '#':
+                            // Register TABLE.FIELD_NAME by text is #TABLE.FIELD_NAME
+                            string field = m.Substring(1).ToUpper().Trim();
+                            if (field.Length > 0)
+                            {
+                                if (m_storeFields.ContainsKey(field))
+                                {
+                                    WebSocketService val;
+                                    m_storeFields.TryRemove(field, out val);
+                                }
+                                m_storeFields.TryAdd(field, this);
+                                lock (m_storeIDs) m_storeIDs += ";" + this.WebSocketContext.SecWebSocketKey;
+                            }
+                            break; 
+                    }
+                    break;
             }
         }
 
@@ -87,7 +183,7 @@ namespace Api.Dynamic
         public override void OnMessage(Byte[] buffer) { }
         protected override void OnClose()
         {
-            lock (storeIDs) storeIDs.Replace(";" + this.WebSocketContext.SecWebSocketKey, string.Empty);
+            lock (m_storeIDs) m_storeIDs.Replace(";" + this.WebSocketContext.SecWebSocketKey, string.Empty);
             base.OnClose();
         }
         protected override void OnError()
---
+++ Api.Dynamic/table1col1Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
+
+namespace Api.Dynamic
+{
+    public class table1col1Controller : ApiController
+    {
+        // GET api/values
+        public IEnumerable<int> Get()
+        {
+            return new int[] { 0, 1 };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "id = " + id;
+        }
+
+        // POST api/values
+        public string Post([FromBody]string value)
+        {
+            return "value = " + value;
+        }
+
+        // PUT api/values/5
+        public string Put(int id, [FromBody]string value)
+        {
+            return "id = " + id + "; value = " + value;
+        }
+
+        // DELETE api/values/5
+        public string Delete(int id)
+        {
+            return "id = " + id;
+        }
+    }
+}
